7f8306ee325a89b217acee0c2a8daad69014ce9e
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-02-13 21:46:43 +0100

    feat: multiple groups in property pane

--- Section 3/WorkingWithPropertyPaneControls/src/webparts/propertyPaneWp/PropertyPaneWpWebPart.ts
+++ Section 3/WorkingWithPropertyPaneControls/src/webparts/propertyPaneWp/PropertyPaneWpWebPart.ts
@@ -157,9 +157,14 @@ export default class PropertyPaneWpWebPart extends BaseClientSideWebPart<IProper
     return {
       pages: [
         {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          displayGroupsAsAccordion: true,
           groups: [
             {
-              groupName: "Product details",
+              groupName: "Group 1",
+              isCollapsed: true,
               groupFields: [
                 PropertyPaneTextField("productname", {
                   label: "Product name",
@@ -275,6 +280,124 @@ export default class PropertyPaneWpWebPart extends BaseClientSideWebPart<IProper
                 })
               ],
             },
+            {
+              groupName: "Group 2",
+              isCollapsed: false,
+              groupFields: [
+                PropertyPaneTextField("productname", {
+                  label: "Product name",
+                  multiline: false,
+                  resizable: false,
+                  deferredValidationTime: 5000,
+                  placeholder: "Please enter product name",
+                  description: "Name property field",
+                }),
+
+                PropertyPaneTextField("productdescription", {
+                  label: "Product description",
+                  multiline: true,
+                  resizable: false,
+                  deferredValidationTime: 5000,
+                  placeholder: "Please enter product description",
+                  description: "Name property field",
+                }),
+
+                PropertyPaneTextField("productcost", {
+                  label: "Product cost",
+                  multiline: false,
+                  resizable: false,
+                  deferredValidationTime: 5000,
+                  placeholder: "Please enter product cost",
+                  description: "Name property field",
+                }),
+
+                PropertyPaneTextField("quantity", {
+                  label: "Product quantity",
+                  multiline: false,
+                  resizable: false,
+                  deferredValidationTime: 5000,
+                  placeholder: "Please enter product quantity",
+                  description: "Name property field",
+                }),
+
+                PropertyPaneToggle('isCertified', {
+                  key: 'isCertified',
+                  label: 'It it Certified?',
+                  onText: 'ISI Certified',
+                  offText: 'Not an ISI Certified Product'
+                }),
+
+                PropertyPaneSlider('rating', {
+                  label: 'Select your rating',
+                  min: 1,
+                  max: 10,
+                  step: 1,
+                  showValue: true,
+                  value: 1
+                }),
+
+                PropertyPaneChoiceGroup('processorType', {
+                  label: 'Choices',
+                  options: [
+                    { key: 'Intel I5', text: 'Intel I5' },
+                    { key: 'Intel I7', text: 'Intel I7', checked: true },
+                    { key: 'Intel I9', text: 'Intel I9' },
+                  ]
+                }),
+
+                PropertyPaneChoiceGroup('invoiceFileType', {
+                  label: 'Select Invoice File Type',
+                  options: [
+                    {
+                      key: 'MSWord', text: 'MSWord',
+                      imageSrc: 'https://img.icons8.com/ios/50/000000/ms-word.png',
+                      imageSize: { width: 32, height: 32 },
+                      selectedImageSrc: 'https://img.icons8.com/color/48/000000/microsoft-word-2019--v2.png'
+                    },
+                    {
+                      key: 'MSExcel', text: 'MSExcel',
+                      imageSrc: 'https://img.icons8.com/ios/50/000000/ms-word.png',
+                      imageSize: { width: 32, height: 32 },
+                      selectedImageSrc: 'https://img.icons8.com/color/48/000000/microsoft-word-2019--v2.png'
+                    },
+                    {
+                      key: 'MSPowerPoint', text: 'MSPowerPoint',
+                      imageSrc: 'https://img.icons8.com/ios/50/000000/ms-word.png',
+                      imageSize: { width: 32, height: 32 },
+                      selectedImageSrc: 'https://img.icons8.com/color/48/000000/microsoft-word-2019--v2.png'
+                    },
+                  ]
+                }),
+
+                PropertyPaneDropdown('newProcessorType', {
+                  label: "New Processor Type",
+                  options: [
+                    { key: 'Intel I5', text: 'Intel I5' },
+                    { key: 'Intel I7', text: 'Intel I7' },
+                    { key: 'Intel I9', text: 'Intel I9' },
+                  ],
+                  selectedKey: 'Intel I7'
+                }),
+
+                PropertyPaneCheckbox('discountCoupon', {
+                  text: 'Do you have a discount coupon?',
+                  checked: false,
+                  disabled: false
+                }),
+
+                PropertyPaneLink('', {
+                  href: 'https://github.com',
+                  text: 'Go to github',
+                  target: '_blank',
+                  popupWindowProps: {
+                    height: 500,
+                    width: 500,
+                    positionWindowPosition: 2,
+                    title: 'github'
+                  }
+                })
+              ],
+            }
           ],
         },
       ],
