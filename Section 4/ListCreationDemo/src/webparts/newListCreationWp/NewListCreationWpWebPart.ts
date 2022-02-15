7cf5a4d614a7386284fc999a9556a740a995b8de
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-02-15 21:34:50 +0100

    feat: UI webpart and bindEvents function

--- Section 4/ListCreationDemo/src/webparts/newListCreationWp/NewListCreationWpWebPart.ts
+++ Section 4/ListCreationDemo/src/webparts/newListCreationWp/NewListCreationWpWebPart.ts
@@ -5,6 +5,7 @@ import {
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { SPHttpClient, SPHttpClientResponse, ISPHttpClientOptions } from '@microsoft/sp-http'
 
 import styles from './NewListCreationWpWebPart.module.scss';
 import * as strings from 'NewListCreationWpWebPartStrings';
@@ -13,49 +14,63 @@ export interface INewListCreationWpWebPartProps {
   description: string;
 }
 
-export default class NewListCreationWpWebPart extends BaseClientSideWebPart <INewListCreationWpWebPartProps> {
+export default class NewListCreationWpWebPart extends BaseClientSideWebPart<INewListCreationWpWebPartProps> {
 
   public render(): void {
     this.domElement.innerHTML = `
-      <div class="${ styles.newListCreationWp }">
-    <div class="${ styles.container }">
-      <div class="${ styles.row }">
-        <div class="${ styles.column }">
-          <span class="${ styles.title }">Welcome to SharePoint!</span>
-  <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-    <p class="${ styles.description }">${escape(this.properties.description)}</p>
-      <a href="https://aka.ms/spfx" class="${ styles.button }">
-        <span class="${ styles.label }">Learn more</span>
-          </a>
-          </div>
-          </div>
-          </div>
-          </div>`;
+      <div class="${styles.newListCreationWp}">
+
+        <h3>Creating a new list dinamically</h3><br/><br/>
+        
+        <p>Please fill out the below details to create a new list programatically</p><br/><br/>
+
+        New List Name: <br/><input type='text' id='txtNewListName'/><br/><br/>
+
+        New List Description: <br/><input type='text' id='txtNewListDescription'/><br/><br/>
+
+        <input type='button' id='btnCreateNewList' value='Create a new List'/><br/>
+
+      </div>`;
+
+    this.bindEvents();
+  }
+
+  private bindEvents(): void {
+    this.domElement
+      .querySelector('#btnCreateNewList')
+      .addEventListener('click', () => { this.createNewList(); })
+  }
+
+  private createNewList(): void{
+
+    let newListName = document.getElementById('txtNewListName')['value'];
+    let txtNewListDescription = document.getElementById('txtNewListDescription')['value'];
+
   }
 
   protected get dataVersion(): Version {
-  return Version.parse('1.0');
-}
+    return Version.parse('1.0');
+  }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-  return {
-    pages: [
-      {
-        header: {
-          description: strings.PropertyPaneDescription
-        },
-        groups: [
-          {
-            groupName: strings.BasicGroupName,
-            groupFields: [
-              PropertyPaneTextField('description', {
-                label: strings.DescriptionFieldLabel
-              })
-            ]
-          }
-        ]
-      }
-    ]
-  };
-}
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
 }
