8f0f36987b69dc9f9342526179ba744b629c260a
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-02-20 22:38:30 +0100

    Refactor CRUDDemo

--- Section 6/CRUDDemo/src/webparts/cruddemo/CruddemoWebPart.ts
+++ Section 6/CRUDDemo/src/webparts/cruddemo/CruddemoWebPart.ts
@@ -18,19 +18,7 @@ export default class CruddemoWebPart extends BaseClientSideWebPart <ICruddemoWeb
   public render(): void {
     this.domElement.innerHTML = `
       <div class="${ styles.cruddemo }">
-    <div class="${ styles.container }">
-      <div class="${ styles.row }">
-        <div class="${ styles.column }">
-          <span class="${ styles.title }">Welcome to SharePoint!</span>
-  <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-    <p class="${ styles.description }">${escape(this.properties.description)}</p>
-      <a href="https://aka.ms/spfx" class="${ styles.button }">
-        <span class="${ styles.label }">Learn more</span>
-          </a>
-          </div>
-          </div>
-          </div>
-          </div>`;
+      </div>`;
   }
 
   protected get dataVersion(): Version {
