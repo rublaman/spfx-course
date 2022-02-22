30183226ca20ff05d5f5bdc7a3c8acc3351c5847
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-02-22 16:53:03 +0100

    feat: form created

--- Section 7/sppnpjscruddemo/src/webparts/sppnpjscruddemo/SppnpjscruddemoWebPart.ts
+++ Section 7/sppnpjscruddemo/src/webparts/sppnpjscruddemo/SppnpjscruddemoWebPart.ts
@@ -8,6 +8,7 @@ import { escape } from '@microsoft/sp-lodash-subset';
 
 import styles from './SppnpjscruddemoWebPart.module.scss';
 import * as strings from 'SppnpjscruddemoWebPartStrings';
+import * as pnp from 'sp-pnp-js'
 
 export interface ISppnpjscruddemoWebPartProps {
   description: string;
@@ -17,20 +18,61 @@ export default class SppnpjscruddemoWebPart extends BaseClientSideWebPart <ISppn
 
   public render(): void {
     this.domElement.innerHTML = `
-      <div class="${ styles.sppnpjscruddemo }">
-    <div class="${ styles.container }">
-      <div class="${ styles.row }">
-        <div class="${ styles.column }">
-          <span class="${ styles.title }">Welcome to SharePoint!</span>
-  <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-    <p class="${ styles.description }">${escape(this.properties.description)}</p>
-      <a href="https://aka.ms/spfx" class="${ styles.button }">
-        <span class="${ styles.label }">Learn more</span>
-          </a>
-          </div>
-          </div>
-          </div>
-          </div>`;
+      <div>
+        <div>
+          <table border='5' bgcolor='aqua'>
+            <tr>
+              <td>Please Enter Software ID </td>
+              <td><input type='text' id='txtID' />
+              <td><input type='submit' id='btnRead' value='Read Details' /></td>
+            </tr>
+            <tr>
+              <td>Software Title</td>
+              <td><input type='text' id='txtSoftwareTitle' />
+            </tr>
+
+            <tr>
+              <td>Software Name</td>
+              <td><input type='text' id='txtSoftwareName' />
+            </tr>
+
+            <tr>
+              <td>Software Vendor</td>
+              <td>
+                <select id="ddlSoftwareVendor">
+                  <option value="Microsoft">Microsoft</option>
+                  <option value="Sun">Sun</option>
+                  <option value="Oracle">Oracle</option>
+                  <option value="Google">Google</option>
+                </select>  
+              </td>
+            </tr>
+
+            <tr>
+              <td>Software Version</td>
+              <td><input type='text' id='txtSoftwareVersion' />
+            </tr>
+
+            <tr>
+              <td>Software Description</td>
+              <td><textarea rows='5' cols='40' id='txtSoftwareDescription'> </textarea> </td>
+            </tr>
+
+            <tr>
+              <td colspan='2' align='center'>
+              <input type='submit'  value='Insert Item' id='btnSubmit' />
+              <input type='submit'  value='Update' id='btnUpdate' />
+              <input type='submit'  value='Delete' id='btnDelete' />
+              <input type='submit'  value='Show All Records' id='btnReadAll' />
+            </td>
+          </table>
+        </div>
+        <div id="divStatus"/>
+
+        <h2>Get All List Items</h2>
+        <hr/>
+        <div id="spListData" />
+      </div>`;
   }
 
   protected get dataVersion(): Version {
