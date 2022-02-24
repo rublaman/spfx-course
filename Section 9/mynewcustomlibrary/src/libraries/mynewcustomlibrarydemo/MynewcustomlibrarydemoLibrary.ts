7bc940ffa1d2eb57ca1bb8e2a50c42688e42f420
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-02-24 14:01:43 +0100

    feat(library): function getCurrentTime() created

--- Section 9/mynewcustomlibrary/src/libraries/mynewcustomlibrarydemo/MynewcustomlibrarydemoLibrary.ts
+++ Section 9/mynewcustomlibrary/src/libraries/mynewcustomlibrarydemo/MynewcustomlibrarydemoLibrary.ts
@@ -2,4 +2,14 @@ export class MynewcustomlibrarydemoLibrary {
   public name(): string {
     return 'MynewcustomlibrarydemoLibrary';
   }
+
+  public getCurrentTime(): string {
+    let currentDate: Date = new Date();
+    let str: string;
+
+    str = "<br>Today date is : ", currentDate.toDateString();
+    str += "<br>Current time is :", currentDate.toTimeString();
+
+    return str;
+  }
 }
