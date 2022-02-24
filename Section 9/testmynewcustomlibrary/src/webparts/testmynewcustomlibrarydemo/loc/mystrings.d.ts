f4df54467294bf2573a5e37972ef9525f7ac3d77
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-02-24 15:50:21 +0100

    testmynewcustomlibrary created

---
+++ Section 9/testmynewcustomlibrary/src/webparts/testmynewcustomlibrarydemo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITestmynewcustomlibrarydemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TestmynewcustomlibrarydemoWebPartStrings' {
+  const strings: ITestmynewcustomlibrarydemoWebPartStrings;
+  export = strings;
+}
